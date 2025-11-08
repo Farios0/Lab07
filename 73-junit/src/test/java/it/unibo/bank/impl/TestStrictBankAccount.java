26155e58b49e22fd474b14ad7466230b5b128b5f
Geremia F. Conca <geremia.conca@studio.unibo.it>
2025-11-08 18:15:23 +0100

    removed a warning

--- 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.function.Executable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
 
 /**
  * Test class for the {@link StrictBankAccount} class.
